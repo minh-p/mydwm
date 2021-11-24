c05a18bd2e4734ead7596b78d89793dbebbb3f17
minh-p <minhisverynormal@gmail.com>
2021-11-23 21:20:12 -0800

    patches

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+exec dwmblocks &
 exec picom --experimental-backend &
 exec nitrogen --restore &
+
+# For vim capslock esc mapping and setting key rate.
+xset r rate 500 50 &
+xmodmap ~/.config/.xmodmapping &
