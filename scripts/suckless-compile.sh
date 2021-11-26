65040b06b968a7d068992803cf5aaf73f6a958ee
minh-p <minhisverynormal@gmail.com>
2021-11-25 19:24:37 -0800

    created a script to compile every suckless software

---
+++ scripts/suckless-compile.sh
@@ -0,0 +1,6 @@
+cd ~/.local/src/dwm && sudo make clean install
+cd ~/.local/src/dwmblocks && sudo make clean install
+cd ~/.local/src/dmenu && sudo make clean install
+cd ~/.local/src/st && sudo make clean install
+cd ~/.local/src/surf && sudo make clean install
+cd ~/.local/src/tabbed && sudo make clean install
