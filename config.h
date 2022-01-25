057c9ff79d594121f298e147478a42a80bb4183e
minh-p <minhisverynormal@gmail.com>
2022-01-25 15:49:00 -0800

    config.h font size

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Font Awesome 5 Free:size=14" };
-static const char dmenufont[]       = "Source Code Pro:size=13";
+static const char *fonts[]          = { "Font Awesome 5 Free:size=11" };
+static const char dmenufont[]       = "Source Code Pro:size=10";
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "" };
