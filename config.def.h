95d1eb99876266686d2e1e7537b1bb17d3eee90a
minh-p <minhisverynormal@gmail.com>
2022-01-29 12:07:18 -0800

    dunst

--- config.def.h
+++ config.def.h
@@ -2,16 +2,16 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-//static const unsigned int gappx     = 20;        /* gaps between windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int gappx     = 15;        /* gaps between windows */
+//static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Monoid Regular Nerd Font Complete:size=11", "Font Awesome 5 Free:size=14" };
-static const char dmenufont[]       = "Source Code Pro:size=12";
+static const char *fonts[]          = { "Font Awesome 5 Free:size=11" };
+static const char dmenufont[]       = "Source Code Pro:size=10";
 
 /* tagging */
-static const char *tags[] = { ">_", "www", "", "4", "5", "6" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,9 +42,9 @@ static void (*bartabfloatfns[])(Monitor *) = { NULL /* , customlayoutfn */ };
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,4 +2,5 @@
 exec dwmblocks &
 exec picom --experimental-backends &
 exec nitrogen --restore &
+exec dunst &
 exec ~/.local/src/dwm/scripts/mappings.sh
