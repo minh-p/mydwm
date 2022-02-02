44545878156cec2bb7d186d5492e4c778fed6553
minh-p <minhisverynormal@gmail.com>
2022-02-01 17:26:05 -0800

    bottom bar is king

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 //static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Font Awesome 5 Free:size=11" };
 static const char dmenufont[]       = "Source Code Pro:size=10";
 
