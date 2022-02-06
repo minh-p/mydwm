6fef17b66bd2f511160c3abc5a3cc59fc531adc6
minh-p <minhisverynormal@gmail.com>
2022-02-05 21:45:53 -0800

    autostart, adding redshift in

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -3,4 +3,5 @@ exec dwmblocks &
 exec picom --experimental-backends &
 exec nitrogen --restore &
 exec dunst &
+exec redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
 exec ~/.local/src/dwm/scripts/mappings.sh
