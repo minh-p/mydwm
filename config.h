966d18fe991f945139d9a91e6063e7ed3367fbf0
minh-p <minhisverynormal@gmail.com>
2022-02-18 17:43:09 -0800

    showbar

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 //static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Font Awesome 6 Free:pixelsize=11" };
 static const char dmenufont[]       = "Source Code Pro:size=10";
