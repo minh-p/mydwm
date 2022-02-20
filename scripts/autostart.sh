9acac94b3de7722869993ddacb2672e6df30dfe8
minh-p <minhisverynormal@gmail.com>
2022-02-19 20:35:28 -0800

    important autostart functions for restarting

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # exec dwmblocks &
+exec pkill slstatus &
 exec slstatus &
 exec picom --experimental-backends &
 exec nitrogen --restore &
+exec pkill dunst &
 exec dunst &
-exec killall redshift
+exec ~/.local/src/dwm/scripts/mappings.sh &
+exec pkill redshift &
 exec redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
-exec ~/.local/src/dwm/scripts/mappings.sh
