40b1edbf88e338c471f954ec8261556c4a126154
minh-p <minhisverynormal@gmail.com>
2022-02-19 20:26:36 -0800

    restart signal patch

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,5 +4,6 @@ exec slstatus &
 exec picom --experimental-backends &
 exec nitrogen --restore &
 exec dunst &
+exec killall redshift
 exec redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
 exec ~/.local/src/dwm/scripts/mappings.sh
