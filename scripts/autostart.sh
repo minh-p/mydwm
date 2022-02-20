e79403a9a4d5d7cf4a1cdb8c3bd544121b706de7
minh-p <minhisverynormal@gmail.com>
2022-02-19 20:46:16 -0800

    fix autostart script

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # exec dwmblocks &
-exec pkill slstatus &
-exec slstatus &
+exec killall slstatus &
+exec pkill redshift &
+exec pkill dunst &
 exec picom --experimental-backends &
 exec nitrogen --restore &
-exec pkill dunst &
 exec dunst &
 exec ~/.local/src/dwm/scripts/mappings.sh &
-exec pkill redshift &
 exec redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
+exec slstatus &
