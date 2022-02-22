5ce7cbdc87692202a32e657986e2821e01d593dd
minh-p <minhisverynormal@gmail.com>
2022-02-21 19:03:11 -0800

    slstatus autostarting problems

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # exec dwmblocks &
-exec killall slstatus &
 exec pkill redshift &
 exec pkill dunst &
 exec picom --experimental-backends &
 exec nitrogen --restore &
 exec dunst &
 exec ~/.local/src/dwm/scripts/mappings.sh &
-exec redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
 exec slstatus &
+exec redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
