d34862d5d9a3650189d8a1dd791d3dd02552c7ab
minh-p <minhisverynormal@gmail.com>
2022-02-21 19:41:58 -0800

    fixed autostart issue

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
+check_process_killall() {
+    [ "$1" = "" ]  && return 0
+    [ `pgrep -n $1` ] && killall $1 || return 0
+}
+
+check_process_pkill() {
+    [ "$1" = "" ]  && return 0
+    [ `pgrep -n $1` ] && killall $1 || return 0
+}
+
 # exec dwmblocks &
-exec pkill redshift &
-exec pkill dunst &
+check_process_killall slstatus
+check_process_pkill redshift
+check_process_pkill dunst
+
 exec picom --experimental-backends &
 exec nitrogen --restore &
 exec dunst &
